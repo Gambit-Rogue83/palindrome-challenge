7ce515f4a45032493636f61d0b7c0e23323c875d
Shattuck Kuzmic <shattuckkuzmic@gmail.com>
2023-11-27 08:09:30 -0800

    challenge completed

--- isPalindrome.js
+++ isPalindrome.js
@@ -1,21 +1,51 @@
 // Write your code below
 
 
-word = 'tacocat'
 
-const isPalindrome = word =>{
-    let backWordArr = []
-    for(i = 0; i < word.length; i++){
-        backWordArr.unshift(word[i])
-    }
-    backWord = backWordArr.splice()
-    if(word = backWord){
-        return true
-    } else {
+//FIRST FAILED ATTEMPT
+// const isPalindrome = word =>{
+//     let backWordArr = []
+//     for(i = 0; i < word.length; i++){
+//         backWordArr.unshift(word[i])
+//     }
+//     backWord = backWordArr.splice()
+//     if(word = backWord){
+//         return true
+//     } else {
+//         return false
+//     }
+// }
+
+// console.log(isPalindrome('racecart'))
+
+
+// word = 'racecar'
+
+//SECOND ATTEMPT
+// const isPalindrome = word =>{
+//     for(i = 0; i < word.length; i++){
+//         for(j = word.length - 1; j < word.length; j--){
+//             if(word[j] === word[i]){
+//                 continue
+//             } else {
+//                 return true
+//             }
+//         }
+//     }
+// }
+
+// console.log(isPalindrome(word))
+
+
+function isPalindrome(word) {
+    let j = word.length - 1
+    for (let i = 0; i < (word.length - 1) / 2; i++) {
+      if (word[i] !== word[j]) {
         return false
+      }
+      j--
     }
-}
+    return true
+  }
 
-console.log(isPalindrome('penis'))
-
-
+  console.log(isPalindrome('tacocaT'))
